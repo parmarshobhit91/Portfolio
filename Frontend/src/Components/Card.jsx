a77b160099d9e927c22462a1402ddff767718fae
Shobhit <parmarshobhit91@gmail.com>
2025-02-16 23:15:10 +0530

    Design changes in card component and project page

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from "react";
 
-function Card({ title, description, image}) {
+function Card({ title, description, image, githublink }) {
   return (
-    <div className=''>
-         <div className='flex flex-col justify-center items-center h-[370px] w-[405px] bg-[rgba(255,255,255,0.9)] shadow-md rounded-xl'>   
-            {image && <img className="w-[80px] h-[80px] object-cover rounded-xl" src={image} alt={title} />}
-            <h1 className='text-2xl font-semibold text-gray-800 mt-4'>{title}</h1>
-                <p className='text-xl font-medium text-gray-600 mt-1'>{description}</p>
-         </div>
+    <div className="flex flex-col justify-center items-center h-[370px] w-[405px] bg-[rgba(255,255,255,0.9)] shadow-md rounded-xl p-4 overflow-hidden">
+    {image && (
+        <img
+          className="w-[80px] h-[80px] object-cover rounded-xl max-w-full"
+          src={image}
+          alt={title}
+        />
+      )}
+      <h1 className="text-2xl font-semibold text-gray-800 mt-4 text-center">
+        {title}
+      </h1>
+      <p className="text-xl font-medium text-gray-600 m-1 text-center overflow-hidden text-ellipsis">
+        {description}
+      </p>
+      <div className="mt-2 w-full text-center">
+        <a
+          href={githublink}
+          className="text-xl font-medium text-blue-600 m-1 truncate block"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {githublink}
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- Frontend/src/Pages/Projects.jsx
+++ Frontend/src/Pages/Projects.jsx
@@ -1,34 +1,50 @@
 import React from 'react'
 import Card from '../Components/Card'
-
+import portfolioimg from "../assets/portfolioimg.png"
 
 const cardData = [
     {
-        title: 'Card 1',
-        description: 'This is card 1',
-        image: 'https://via.placeholder.com/150'
+        title: 'Portfolio',
+        description: 'My personal portfolio website using MERN Stack',
+        image: portfolioimg,
+        githublink: 'https://github.com/parmarshobhit91/Portfolio'
+        
     },
     {
         title: 'Card 2',
         description: 'This is card 2',
-        image: 'https://via.placeholder.com/150'
+        image: 'https://via.placeholder.com/150',
+        githublink: 'https://via.placeholder.com/150'
     },
     {
         title: 'Card 3',
         description: 'This is card 3',
-        image: 'https://via.placeholder.com/150'
+        image: 'https://via.placeholder.com/150',
+        githublink: 'https://via.placeholder.com/150'
     },
     {
         title: 'Card 4',
         description: 'This is card 4',
-        image: 'https://via.placeholder.com/150'
+        image: 'https://via.placeholder.com/150',
+        githublink: 'https://via.placeholder.com/150'
     },
 ]
 
 
 function Projects() {
     return (
-        <div className='h-screen sm:h-[80vh] md:h-[90vh] lg:h-[90vh] p-8'>
+        // <div className='h-screen sm:h-[80vh] md:h-[90vh] lg:h-[90vh] p-8'>
+        //     <div className='p-8'>
+        //         <h4 className='text-xl font-bold text-left mt-8'>Projects</h4>
+        //         <h1 className='text-4xl font-bold text-left mt-8'>Projects done by me</h1>
+        //         <div className='flex flex-wrap justify-start gap-8 mx-auto p-6'>
+        //             {cardData.map((item) => (
+        //                 <Card key={item.title} title={item.title} {...item} />
+        //             ))}
+        //         </div>
+        //     </div>
+        // </div>
+         <div className='h-screen sm:h-[80vh] md:h-[90vh] lg:h-[90vh] p-8'>
             <div className='p-8'>
                 <h4 className='text-xl font-bold text-left mt-8'>Projects</h4>
                 <h1 className='text-4xl font-bold text-left mt-8'>Projects done by me</h1>
